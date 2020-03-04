21cdf2adb832eef70ab8c3bf08d90149100a799f
Mikhail Kalinin <m.kalinin@selectel.ru>
2020-03-04 21:53:17 +0300

    Load releases for last 30 days from the array (mock api)

--- src/pages/discover/last-30-days.vue
+++ src/pages/discover/last-30-days.vue
@@ -7,7 +7,9 @@
         </div>
         <div class="row">
             <release
+                v-for="release in releases"
                 :release="release"
+                :key="release.id"
             ></release>
         </div>
     </div>
@@ -19,19 +21,29 @@ import release from '../../components/release.vue'
 export default {
     data() {
         return {
-            "release": {
-                "spotify_id": "",
-                "deezer_id": "",
-                "title": "All That Matters (feat. John Harris)",
-                "type": "album",
-                "released": "2020-03-06T00:00:00Z",
-                "itunes_id": "1499647989",
-                "id": 7002643,
-                "artist_name": "Frontliner",
-                "explicit": false,
-                "poster": "https://is2-ssl.mzstatic.com/image/thumb/Music124/v4/2d/03/52/2d035219-1233-6319-84c1-a05790f28d22/8718857967682.png/500x500bb.jpeg",
-                "artist_id": 227062
-            }
+            releases: []
+        }
+    },
+    created() {
+        this.getReleases();
+    },
+    methods: {
+        getReleases() {
+            this.releases = [
+                {
+                    "spotify_id": "",
+                    "deezer_id": "",
+                    "title": "All That Matters (feat. John Harris)",
+                    "type": "album",
+                    "released": "2020-03-06T00:00:00Z",
+                    "itunes_id": "1499647989",
+                    "id": 7002643,
+                    "artist_name": "Frontliner",
+                    "explicit": false,
+                    "poster": "https://is2-ssl.mzstatic.com/image/thumb/Music124/v4/2d/03/52/2d035219-1233-6319-84c1-a05790f28d22/8718857967682.png/500x500bb.jpeg",
+                    "artist_id": 227062
+                },
+            ]
         }
     },
     components: {
