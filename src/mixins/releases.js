493a56cc3919bc14f8db66aa8d2884ac33448443
Mikhail Kalinin <m.kalinin@selectel.ru>
2020-03-07 01:28:38 +0300

    Extract loadReleases method

--- src/mixins/releases.js
+++ src/mixins/releases.js
@@ -8,26 +8,25 @@ export default {
         this.resource = this.$resource("releases");
     },
     methods: {
-        loadPastMounthReleases() {
-            var since = "2020-02-01"
-            var till = "2020-02-10"
+        loadReleases(since, till) {
             return this.resource.get({"since": since, "till": till}).then(
                 resp => this.releases = resp.body
             );
+        },
+        loadPastMounthReleases() {
+            var since = "2020-02-01"
+            var till = "2020-02-10"
+            return this.loadReleases(since, till);
         },
         loadWeeklyReleases() {
             var since = "2020-03-01"
             var till = "2020-03-10"
-            return this.resource.get({"since": since, "till": till}).then(
-                resp => this.releases = resp.body
-            );
+            return this.loadReleases(since, till);
         },
         loadNextWeekReleases() {
             var since = "2020-03-10"
             var till = "2020-03-17"
-            return this.resource.get({"since": since, "till": till}).then(
-                resp => this.releases = resp.body
-            );
+            return this.loadReleases(since, till);
         }
     },
     computed: {
