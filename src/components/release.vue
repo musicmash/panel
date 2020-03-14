81fdbaecd9e56db51bcda4fdecf61218d31c47da
Mikhail Kalinin <m.kalinin@selectel.ru>
2020-03-15 00:29:32 +0300

    Add link to open release on Spotify

--- src/components/release.vue
+++ src/components/release.vue
@@ -10,6 +10,20 @@
                         </svg>
                     </a>
                 </li>
+                <li class="action-item" v-if="releaseExistsOnSpotify">
+                    <a class="action-item-btn" :href="linkOnSpotify">
+                        <svg focusable="false" aria-hidden="true" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+                            <path d="M19.098 10.638c-3.868-2.297-10.248-2.508-13.941-1.387-.593.18-1.22-.155-1.399-.748-.18-.593.154-1.22.748-1.4 4.239-1.287 11.285-1.038 15.738 1.605.533.317.708 1.005.392 1.538-.316.533-1.005.709-1.538.392zm-.126 3.403c-.272.44-.847.578-1.287.308-3.225-1.982-8.142-2.557-11.958-1.399-.494.15-1.017-.129-1.167-.623-.149-.495.13-1.016.624-1.167 4.358-1.322 9.776-.682 13.48 1.595.44.27.578.847.308 1.286zm-1.469 3.267c-.215.354-.676.465-1.028.249-2.818-1.722-6.365-2.111-10.542-1.157-.402.092-.803-.16-.895-.562-.092-.403.159-.804.562-.896 4.571-1.045 8.492-.595 11.655 1.338.353.215.464.676.248 1.028zm-5.503-17.308c-6.627 0-12 5.373-12 12 0 6.628 5.373 12 12 12 6.628 0 12-5.372 12-12 0-6.627-5.372-12-12-12z"/>
+                        </svg>
+                    </a>
+                </li>
+                <li class="action-item" v-if="releaseExistsOnSpotify">
+                    <a class="action-item-btn" :href="linkOnSpotify">
+                        <svg class="svg-icon svg-icon-play" focusable="false" height="1em" width="1em" viewBox="0 0 12 12" aria-hidden="true">
+                            <path fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M2.5.5v11l9-5.5z"></path>
+                        </svg>
+                    </a>
+                </li>
             </ul>
             <!-- <img class="release-poster-blur" :src="release.poster|resizeImage('76x76')" alt=""> -->
         </div>
@@ -54,6 +68,12 @@ export default {
         },
         linkOnAppleMusic: function() {
             return `itmss://music.apple.com/us/${this.release.type}/${this.release.itunes_id}`;
+        },
+        releaseExistsOnSpotify: function() {
+            return this.release.spotify_id != "";
+        },
+        linkOnSpotify: function() {
+            return `spotify:album:${this.release.spotify_id}`;
         }
   },
   filters: {
