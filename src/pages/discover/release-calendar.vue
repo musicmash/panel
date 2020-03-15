3e17cd5c22bea0abc51d35720baa839f5f45aae4
Mikhail Kalinin <m.kalinin@selectel.ru>
2020-03-15 03:05:21 +0300

    Fix release-calendar page layout

--- src/pages/discover/release-calendar.vue
+++ src/pages/discover/release-calendar.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
         <div class="row">
-            <h1 class="heading">Release calendar</h1>
+            <div class="col-sm-12 col-lg-12">
+                <h1 class="heading">Release calendar</h1>
+            </div>
         </div>
         <div class="row">
-            <nav class="navbar navbar-expand p-0 ml-2 pb-2">
+            <nav class="navbar navbar-expand p-0 pb-4">
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <router-link
