09d086df140098bac1e1faae8655124570af83a8
Mikhail Kalinin <m.kalinin@selectel.ru>
2020-04-02 23:51:49 +0300

    Remove navbar background

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid justify-content-between p-0">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
