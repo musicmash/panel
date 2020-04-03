34f47743df72b153f45f31e84d0808bded0787b5
Mikhail Kalinin <m.kalinin@selectel.ru>
2020-04-03 22:45:53 +0300

    Add simple sidebar on all-releases page

---
+++ src/components/sidebar/sidebar.vue
@@ -0,0 +1,140 @@
+<template>
+    <nav class="sidebar" :class="{ active: isActive }">
+        <div class="sidebar-header">
+            <span>Filter releases</span>
+        </div>
+
+        <ul class="list-unstyled components">
+            <li class="active">
+                <a
+                    class="dropdown-toggle"
+                    data-toggle="collapse"
+                    href="#when-dropdown"
+                    role="button"
+                    aria-expanded="false"
+                    aria-controls="when-dropdown"
+                >
+                    When
+                </a>
+                <ul class="collapse list-unstyled show" id="when-dropdown">
+                    <li>
+                        <a href="#" class="mt-2">Last 30 days</a>
+                    </li>
+                    <li>
+                        <a href="#">This week</a>
+                    </li>
+                    <li>
+                        <a href="#">Next week</a>
+                    </li>
+                    <li>
+                        <router-link to="/discover/release-calendar">Calendar</router-link>
+                    </li>
+                </ul>
+            </li>
+            <li class="active mt-3">
+                <a
+                    class="dropdown-toggle"
+                    data-toggle="collapse"
+                    href="#type-dropdown"
+                    role="button"
+                    aria-expanded="false"
+                    aria-controls="type-dropdown"
+                >
+                    Type
+                </a>
+                <ul class="collapse list-unstyled show" id="type-dropdown">
+                    <li>
+                        <a href="#" class="mt-2">Albums</a>
+                    </li>
+                    <li>
+                        <a href="#">Music videos</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "sidebar",
+    data() {
+        return {
+            isActive: false,
+        }
+    },
+    methods: {
+        collapse: function() {
+            this.isActive = !this.isActive;
+        }
+    }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.sidebar {
+    min-width: 250px;
+    max-width: 250px;
+    background: #7386D5;
+    color: #fff;
+    transition: all 0.3s;
+}
+.sidebar.active {
+    margin-left: -250px;
+}
+.sidebar .sidebar-header {
+    padding: 20px;
+    background: #6d7fcc;
+}
+.sidebar ul.components {
+    padding: 20px 0;
+}
+.sidebar ul p {
+    color: #fff;
+    padding: 10px;
+}
+.sidebar ul li a {
+    padding: 10px;
+    font-size: 1.1em;
+    display: block;
+}
+.sidebar ul li a:hover {
+    color: #7386D5;
+    background: #fff;
+}
+.sidebar ul li.active>a, a[aria-expanded="true"] {
+    color: #fff;
+    background: #6d7fcc;
+}
+a[data-toggle="collapse"] {
+    position: relative;
+}
+
+.dropdown-toggle::after {
+    display: block;
+    position: absolute;
+    top: 50%;
+    right: 20px;
+    transform: translateY(-50%);
+}
+
+ul ul a {
+    font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #6d7fcc;
+}
+
+@media (max-width: 768px) {
+    .sidebar {
+        margin-left: -250px;
+    }
+    .sidebar.active {
+        margin-left: 0;
+    }
+    .sidebarCollapse span {
+        display: none;
+    }
+}
+</style>
--- src/pages/all-releases.vue
+++ src/pages/all-releases.vue
@@ -1,19 +1,72 @@
 <template>
-    <h1 class="heading">All releases</h1>
+    <div class="wrapper">
+        <sidebar ref="sidebar" />
+        <div class="content">
+            <nav class="navbar navbar-expand-lg navbar-dark p-0">
+                <div class="navbar-content">
+                    <button
+                        class="btn btn-info"
+                        type="button"
+                        v-on:click="$refs.sidebar.collapse()"
+                    >
+                        Toggle Filters
+                    </button>
+                </div>
+            </nav>
+
+            <div class="releases">
+                <h2>Lorem Ipsum Dolor</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <div class="line"></div>
+
+                <h2>Lorem Ipsum Dolor</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <div class="line"></div>
+
+                <h2>Lorem Ipsum Dolor</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <div class="line"></div>
+
+                <h2>Lorem Ipsum Dolor</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import sidebar from '@/components/sidebar/sidebar'
 
+export default {
+    components: {
+        sidebar
+    },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.heading {
-    margin-top: 20px;
-    margin-bottom: 20px;
-    box-sizing: border-box;
-    font-size: 72px;
-    line-height: 74px;
-    font-weight: 700;
+.wrapper {
+    display: flex;
+    width: 100%;
+    align-items: stretch;
+}
+
+.content {
+    width: 100%;
+    min-height: 100vh;
+    transition: all 0.3s;
+}
+
+.navbar-content {
+    padding-left: 1rem;
+}
+
+.releases {
+    padding: .5rem 1rem;
 }
 </style>
