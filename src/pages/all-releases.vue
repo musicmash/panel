1cbb2203d10c4fcbf753de4dfe58f09653a1a8d5
Mikhail Kalinin <m.kalinin@selectel.ru>
2020-04-10 00:06:16 +0300

    Fix margins on all-releases page

--- src/pages/all-releases.vue
+++ src/pages/all-releases.vue
@@ -2,7 +2,7 @@
     <div class="wrapper">
         <sidebar ref="sidebar" />
         <div class="content">
-            <div class="mx-4 p-0 d-md-none">
+            <div class="mx-3 p-0 d-md-none">
                 <button
                     class="btn btn-info"
                     type="button"
@@ -11,15 +11,16 @@
                     Toggle Filters
                 </button>
             </div>
+
             <div class="p-0">
-                <h1 class="header ml-4 d-inline">{{ filter }}</h1>
+                <h1 class="header ml-3 d-inline">{{ filter }}</h1>
                 <h1 class="sub-header ml-2 d-inline align-center">({{ releases.length }} results)</h1>
             </div>
 
             <div class="releases">
                 <div class="row">
                     <release
-                        class="col-3 mx-4 mb-4 p-0"
+                        class="col-3 mx-3 mb-4 p-0"
                         v-for="release in releases"
                         :release="release"
                         :key="release.id"
