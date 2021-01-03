04bd7c10db75e46bbc170eeed328497b18ac36a1
Mikhail Kalinin <m.kalinin@selectel.ru>
2021-01-04 01:58:42 +0300

    Implement simple fetch-releases logic

--- src/pages/ReleasesPage.vue
+++ src/pages/ReleasesPage.vue
@@ -4,19 +4,38 @@
         <releases-onboarding-banner />
 
         <div class="container">
-            <h1>this is releases</h1>
+            <release
+                class="mr-3 mb-3 p-0"
+                v-for="release in releases"
+                :release="release"
+                :text="release"
+                :key="release.id"
+            >
+            </release>
         </div>
     </div>
 </template>
 
 <script>
+import Release from "@/components/release";
 import ReleasesOnboardingBanner from "@/components/ReleasesOnboardingBanner";
+import { mapState } from "vuex";
 
 export default {
     mounted() {
-        console.log(this.$store.state.releases.items);
+        if (!this.$store.state.releases.isLoaded)
+            this.fetchReleases();
+    },
+    computed: mapState({
+        releases: (state) => state.releases.items,
+    }),
+    methods: {
+        fetchReleases() {
+            return this.$store.dispatch("releases/fetch");
+        },
     },
     components: {
+        "release": Release,
         "releases-onboarding-banner": ReleasesOnboardingBanner,
     },
 };
