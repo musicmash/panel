6248c4336ea8ba2a57bdc3a674ff0b1452b46775
Mikhail Kalinin <m.kalinin@selectel.ru>
2021-01-09 00:23:40 +0300

    Remove vue-axios cause it's useless

--- src/common/api.service.js
+++ src/common/api.service.js
@@ -1,40 +1,37 @@
-import Vue from "vue";
 import axios from "axios";
-import VueAxios from "vue-axios";
 
 const ApiService = {
     init() {
-        Vue.use(VueAxios, axios);
-        Vue.axios.defaults.baseURL = "/v1/";
-        Vue.axios.defaults.withCredentials = true;
+        axios.defaults.baseURL = "/v1/";
+        axios.defaults.withCredentials = true;
     },
 
     query(resource, params) {
-        return Vue.axios.get(resource, params).catch((error) => {
+        return axios.get(resource, params).catch((error) => {
             throw new Error(`ApiService ${error}`);
         });
     },
 
     get(resource, slug = "") {
-        return Vue.axios.get(`${resource}/${slug}`).catch((error) => {
+        return axios.get(`${resource}/${slug}`).catch((error) => {
             throw new Error(`ApiService ${error}`);
         });
     },
 
     post(resource, params) {
-        return Vue.axios.post(`${resource}`, params);
+        return axios.post(`${resource}`, params);
     },
 
     update(resource, slug, params) {
-        return Vue.axios.put(`${resource}/${slug}`, params);
+        return axios.put(`${resource}/${slug}`, params);
     },
 
     put(resource, params) {
-        return Vue.axios.put(`${resource}`, params);
+        return axios.put(`${resource}`, params);
     },
 
     delete(resource) {
-        return Vue.axios.delete(resource).catch((error) => {
+        return axios.delete(resource).catch((error) => {
             throw new Error(`ApiService ${error}`);
         });
     },
