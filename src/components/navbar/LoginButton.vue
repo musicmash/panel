c7c7532f7d8520b00066f6a55c8c13dbbef48aef
Mikhail Kalinin <m.kalinin@selectel.ru>
2021-01-09 04:27:53 +0300

    Fix linter

--- src/components/navbar/LoginButton.vue
+++ src/components/navbar/LoginButton.vue
@@ -12,14 +12,20 @@
 export default {
     methods: {
         getAuthUrl() {
-            const url = new URL('https://accounts.spotify.com/authorize');
-            url.searchParams.append("client_id", "e8a13cdace274204bd3f7c8526f00361");
-            url.searchParams.append("redirect_uri", `https://${window.location.hostname}/v1/callbacks/spotify/auth`);
+            const url = new URL("https://accounts.spotify.com/authorize");
             url.searchParams.append("response_type", "code");
             url.searchParams.append("show_dialog", "true");
             url.searchParams.append("state", "auth");
+            url.searchParams.append(
+                "client_id",
+                "e8a13cdace274204bd3f7c8526f00361"
+            );
+            url.searchParams.append(
+                "redirect_uri",
+                `https://${window.location.hostname}/v1/callbacks/spotify/auth`
+            );
             return url.href;
-        }
+        },
     },
 };
 </script>
